9718524ba9a23b0610a5d468c1a74babf4d3ad07
xKow <49077814+xKow@users.noreply.github.com>
2021-04-08 11:28:46 +0200

    Add files via upload

---
+++ commands/serverinfo.js
@@ -0,0 +1,7 @@
+const discord = require('discord.js')
+const fivereborn = require('fivereborn-query')
+
+module.exports = {
+    name: "serverinfo",
+    description: "Få information om serveren"
+}
