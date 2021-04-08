136f65bc65338a76c6f364e85b80351156ef8ceb
xKow <49077814+xKow@users.noreply.github.com>
2021-04-08 11:52:40 +0200

    Update index.js
    en faktisk virkende index.js som er tjekket igennem for fejl :)

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const discord = require('discord.js')
+const discord = require('discord.js');
 const fs = require('fs');
-const client = new discord.Client()
-const config = require('./config.json')
-const commandHandler = require("./command")
+const fivereborn = require('fivereborn-query');
+const client = new discord.Client();
+const config = require('./config.json');
 client.commands = new discord.Collection();
-const fivereborn = require('fivereborn-query')
+
+prefix = '.'
 
 client.on('ready', () => {
     console.log(`Logged ind som ${client.user.username}. Klar på ${client.guilds.cache.size} servers, med et total af ${client.users.cache.size} users`)
-}),
+    function activity(){
+        setTimeout(() => { 
+            fivereborn.query(`${config.ip}`,`${config.port}`, (error, data) => {
+                if (error) { 
+                    console.log(error);
+                } else {
+                    client.user.setActivity(`${data.clients}/${data.maxclients} online`, { type: "WATCHING" });
+                }
+            });
+            activity(); 
+        }, 5000);
+    };
+    activity(); 
+})
 
-function activity(){
-    setTimeout(() => { 
-        fivereborn.query(`${config.ip}`,`${config.port}`, (error, data) => {
-            if (error) { 
-                console.log(error);
-            } else {
-                client.user.setActivity(`${data.clients}/${data.maxclients} online`, { type: "COMPETING" });
-            }
-        });
-        activity(); 
-    }, 1000);
-};
-activity();
+
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles){
